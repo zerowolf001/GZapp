65e5aaff298f3c2c00030dd41b456fb68d18a176
zhangzhenhua <200932269@qq.com>
2017-08-02 08:31:00 +0800

    New Pages

---
+++ src/page/bed.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <head-top head-title="患者详情" go-back='true'></head-top>
+    <section class="bed_name">
+      <a href=""><img src="../assets/icon/woman.png" alt=""></a>
+    </section>
+  </div>
+</template>
+
+<script>
+  import headTop from '../components/head'
+  import footGuide from '../components/footGuide'
+
+  export default {
+    data () {
+      return {}
+    },
+    components:{
+      headTop,
+      footGuide
+    },
+    computed: {
+
+    },
+    watch: {
+
+    }
+  }
+</script>
+<style>
+  .bed_name {
+    background:#fff;
+    box-shadow:0 1px 3px #ddd;
+    padding-top: 1.95rem;
+  }
+</style>
