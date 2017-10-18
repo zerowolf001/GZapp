11b6ab2aa236d8fe9fb385d17ceec3ad8f8bb0b4
xjz <200932269@qq.com>
2017-10-18 10:24:43 +0800

    新增news列表

--- src/page/news.vue
+++ src/page/news.vue
@@ -6,38 +6,10 @@
         <img src="http://www.gzsrmyy.com/skin/images/dj_banner.jpg" alt="">
       </div>
       <ul>
-        <li v-for="item in EXData">
+        <router-link :to="{path: 'newspage', query:{id: item.xh}}"  v-for="item in EXData" :key="item.xh" tag="li">
           <h4>[{{item.type}}]{{item.title}}</h4>
           <p>{{item.title}}</p>
-        </li>
-        <!--<li>
-          <h4>[公告]赣州市人民医院2017年8月招聘考试拟录用人员公示</h4>
-          <p>我院2017年8月招聘编外人员拟录用名单公示如下...</p>
-        </li>
-        <li>
-          <h4>[信息]我院召开全面推开城市公立医院综合改革启动部署暨周例会</h4>
-          <p>我院在南院科教楼二楼1号学术报告厅召开全面推开城市公立医院综合改革启动部署暨周例会。</p>
-        </li>
-        <li>
-          <h4>[信息]我院城市公立医院综合改革正式启动</h4>
-          <p>我院城市公立医院综合改革正式启动，系统顺利完成切换并准时上线投入使用。</p>
-        </li>
-        <li>
-          <h4>[信息]我院城市公立医院综合改革正式启动</h4>
-          <p>我院城市公立医院综合改革正式启动，系统顺利完成切换并准时上线投入使用。</p>
-        </li>
-        <li>
-          <h4>[信息]我院城市公立医院综合改革正式启动</h4>
-          <p>我院城市公立医院综合改革正式启动，系统顺利完成切换并准时上线投入使用。</p>
-        </li>
-        <li>
-          <h4>[信息]我院城市公立医院综合改革正式启动</h4>
-          <p>我院城市公立医院综合改革正式启动，系统顺利完成切换并准时上线投入使用。</p>
-        </li>
-        <li>
-          <h4>[信息]我院城市公立医院综合改革正式启动</h4>
-          <p>我院城市公立医院综合改革正式启动，系统顺利完成切换并准时上线投入使用。</p>
-        </li>-->
+        </router-link>
       </ul>
     </div>
     <foot-guide></foot-guide>
@@ -51,7 +23,8 @@
   export default {
     data(){
       return {
-        EXData:null,
+        EXData:[],
+        StationID:'0397',
       }
     },
     mounted(){
