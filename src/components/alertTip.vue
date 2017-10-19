a40dee5fbe77040722cda2589eb50403dd76d7ce
xjz <200932269@qq.com>
2017-10-19 15:22:18 +0800

    update

---
+++ src/components/alertTip.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="MessageToast">
+    <p class="tip_text">{{alertText}}</p>
+  </div>
+</template>
+<script>
+  export default {
+    data(){
+      return{
+      }
+    },
+    props: ['alertText']
+  }
+</script>
